32f7dc31fad9aaae256311fa5d916639f09a3b5a
Francisco J. Castellanos <fcastellanos@dlsi.ua.es>
2024-02-23 14:01:17 +0100

    Getting ISMIR repository to ICDAR 2024 competition

---
+++ utilConst.py
@@ -0,0 +1,29 @@
+
+
+AUGMENTATION_FLIPH = "flipH"
+AUGMENTATION_FLIPV = "flipV"
+AUGMENTATION_ROTATION = "rot"
+AUGMENTATION_SCALE = "scale"
+AUGMENTATION_RANDOM = "random"
+AUGMENTATION_NONE = "none"
+
+
+
+
+AUGMENTATION_CHOICES = [
+                AUGMENTATION_NONE,
+                AUGMENTATION_FLIPH,
+                AUGMENTATION_FLIPV,
+                AUGMENTATION_ROTATION,
+                AUGMENTATION_SCALE,
+                AUGMENTATION_RANDOM
+                ]
+
+
+
+
+kPIXEL_VALUE_FOR_MASKING = -1
+kNUMBER_CHANNELS = 3
+
+
+KEY_RESULT="result"
