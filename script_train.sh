f316d07b17672ea390830044fe4205896e06d9a6
Francisco J. Castellanos <fcastellanos@dlsi.ua.es>
2024-04-08 10:49:52 +0000

    updating script for many shot

--- script_train.sh
+++ script_train.sh
@@ -1,16 +1,17 @@
 
 DATASET=$1  # Latin2FS, Latin14396FS, Latin16746FS, Syr341FS
+MODE="MS"
 
-data_train="dataset/U-DIADS-Bib-FS/${DATASET}/training.txt"
-data_val="dataset/U-DIADS-Bib-FS/${DATASET}/validation.txt"
-NPATCHES="2048"
+data_train="dataset/U-DIADS-Bib-${MODE}/${DATASET}/training.txt"
+data_val="dataset/U-DIADS-Bib-${MODE}/${DATASET}/validation.txt"
+NPATCHES="1024"
 
 for layer_name in "BG" "Para" "Deco" "Text" "Title" "Head"; do
 			path_model="models/${DATASET}_${layer_name}.h5"
             output_file="logs/out_${DATASET}_${layer_name}_${options_serial}_npatches${NPATCHES}.txt"
             echo $output_file
 			echo $data_train
-			python3 main.py -m ${path_model} \
+			python3 -u main.py -m ${path_model} \
                 		-db_train_src ${data_train} \
                 		-db_val_src ${data_val} \
                 		-aug random scale rot flipV flipH \
